c7f8acecbc6123bded13f7679a6a2f760b7f12fc
Zhao <siqizhao@ad.helsinki.fi>
2020-11-19 22:35:20 +0200

    human data wrangling

---
+++ create_human.R
@@ -0,0 +1,8 @@
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+str(hd)
+dim(hd)
+str(gii)
+dim(gii)
+summary(hd)
+summary(gii)
