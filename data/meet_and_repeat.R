ee304f68d406a234915087a4f06f5d93b12fb6fd
Zhao <siqizhao@ad.helsinki.fi>
2020-12-05 20:15:26 +0200

    update

--- data/meet_and_repeat.R
+++ data/meet_and_repeat.R
@@ -9,25 +9,36 @@ library(MASS)
 library(readr)
 
 BPRS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", sep  =" ", header = T)
-RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", sep  =" ", header = T)
 
 summary(BPRS)
 str(BPRS)
-summary(RATS)
-str(RATS)
+
 
 
 BPRS$treatment <- factor(BPRS$treatment)
 BPRS$subject <- factor(BPRS$subject)
-RATS$ID <- factor(RATS$ID)
-RATS$Group <- factor(RATS$Group)
+
 
 BPRSL <-  BPRS %>% gather(key = weeks, value = bprs, -treatment, -subject)
 BPRSL <-  BPRSL %>% mutate(week = as.integer(substr(weeks,5,5)))
 
+# read the RATS data
+RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", header = TRUE, sep = '\t')
+
+# Factor variables ID and Group
+RATS$ID <- factor(RATS$ID)
+RATS$Group <- factor(RATS$Group)
+
+# Glimpse the data
+glimpse(RATS)
+
+# Convert data to long form
 RATSL <- RATS %>%
   gather(key = WD, value = Weight, -ID, -Group) %>%
-  mutate(Time = as.integer(substr(WD, 3, 4))) 
+  mutate(Time = as.integer(substr(WD,3,4))) 
+
+# Glimpse the data
+glimpse(RATSL)
 
 
 names(BPRSL)
@@ -38,5 +49,5 @@ names(RATSL)
 str(RATSL)
 summary(RATSL)
 
-write.csv(BPRSL, file = "data/BPRSL.csv")
-write.csv(RATSL, file = "data/RATSL.csv")
+write.csv(BPRSL, file = "BPRSL")
+write.csv(RATSL, file = "RATSL")
